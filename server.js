c664b7942c67efe78c6650776a032687bb6bfbc2
dlsoares18 <dlsoares18@gmail.com>
2024-07-27 23:59:01 +0100

    Commit Teste

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 
 const app = express();
+app.use.json();
 
 app.get('/user', (req, res) => {
     res.send('Hello world.')
@@ -8,7 +9,7 @@ app.get('/user', (req, res) => {
 })
 
 app.post('/user', async(req, res) => {
-    
+    res.statusCode(200).json
 })
 
 app.listen('3000')
