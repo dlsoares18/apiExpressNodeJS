a630e09f1f7ad10abbab5a2a0f83f9c3830b8a57
dlsoares18 <dlsoares18@gmail.com>
2024-08-03 19:01:30 +0100

    mysql configured

--- server.js
+++ server.js
@@ -1,7 +1,23 @@
-import express from 'express';
+const mysql = require('mysql');
+const express = require('express');
 
 const app = express();
-app.use.json();
+app.use(express.json());
+
+const conection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'contactsanta'
+})
+
+conection.connect(err => {
+    if (err) {
+        console.error('Erro ao conectar a base de dados:', err);
+        return;
+    }
+    console.log('Conexão estabelecida!');
+})
 
 app.get('/user', (req, res) => {
     res.send('Hello world.')
@@ -10,6 +26,7 @@ app.get('/user', (req, res) => {
 
 app.post('/user', async(req, res) => {
     res.statusCode(200).json
+    return;
 })
 
 app.listen('3000')
